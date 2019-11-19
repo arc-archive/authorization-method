fc11a9feaa9fd94f529d5b792898a3ba08d8e0a8
Pawel Psztyc <jarrodek@gmail.com>
2019-11-19 13:57:17 -0800

    test: adding basic tests for oauth 2 implicit flow

---
+++ test/oauth2-implicit-method.test.js
@@ -0,0 +1,316 @@
+import { html, fixture, assert, oneEvent, nextFrame } from '@open-wc/testing';
+import { spy } from 'sinon';
+import { tap } from '@polymer/iron-test-helpers/mock-interactions.js';
+import { METHOD_OAUTH2 } from '../index.js';
+import '../authorization-method.js';
+import {
+  oauth2GrantTypes,
+  _setOauth2Defaults
+} from '../src/Oauth2MethodMixin.js';
+
+describe('OAuth 2, implicit method', () => {
+  const redirectUri = 'https://redirect.com/';
+  const inputFields = [
+    ['clientId', '821776164331-rserncqpdsq32lmbf5cfeolgcoujb6fm.apps.googleusercontent.com'],
+    ['authorizationUri', 'https://accounts.google.com/o/oauth2/v2/auth'],
+    ['scopes', ['email', 'profile']],
+  ];
+
+  function createParamsMap() {
+    const props = {
+      redirectUri,
+    };
+    inputFields.forEach(([n, v]) => props[n] = v);
+    return props;
+  }
+
+  async function basicFixture(opts) {
+    opts = opts || {};
+    const {
+      clientId,
+      authorizationUri,
+      redirectUri,
+      scopes,
+    } = opts;
+    return (await fixture(html`<authorization-method
+      type="${METHOD_OAUTH2}"
+      granttype="implicit"
+      .clientId="${clientId}"
+      .authorizationUri="${authorizationUri}"
+      .redirectUri="${redirectUri}"
+      .scopes="${scopes}"></authorization-method>`));
+  }
+
+  describe('DOM rendering', () => {
+    let element;
+    let form;
+    beforeEach(async () => {
+      element = await basicFixture();
+      form = element.shadowRoot.querySelector('form.oauth2-auth');
+    });
+
+    it('has form in the DOM', async () => {
+      assert.ok(form);
+    });
+
+    it('has form has autocomplete', async () => {
+      assert.equal(form.getAttribute('autocomplete'), 'on');
+    });
+
+    inputFields.forEach(([name]) => {
+      it(`form has ${name} input`, async () => {
+        const input = form.querySelector(`*[name="${name}"]`);
+        assert.ok(input);
+      });
+    });
+
+    it('renders redirect URI filed', async () => {
+      element.redirectUri = redirectUri;
+      await nextFrame();
+      const node = element.shadowRoot.querySelector('.redirect-section');
+      assert.ok(node);
+      const label = node.querySelector('.code');
+      assert.equal(label.textContent.trim(), redirectUri);
+    });
+
+    it('does not render token section when no token information', () => {
+      const node = element.shadowRoot.querySelector('.current-token');
+      assert.notOk(node);
+    });
+
+    it('renders token section when token information is set', async () => {
+      element.accessToken = 'test-token';
+      await nextFrame();
+      const node = element.shadowRoot.querySelector('.current-token');
+      assert.ok(node);
+      const label = node.querySelector('.code');
+      assert.equal(label.textContent.trim(), 'test-token');
+    });
+  });
+
+  describe('Advanced mode', () => {
+    it('renders all fields when no initial values', async () => {
+      const element = await basicFixture();
+      assert.isTrue(element.advancedOpened, 'advanced view is opened');
+      assert.isUndefined(element.isAdvanced, 'isAdvanced is undefined');
+    });
+
+    it('does not render advanced switch', async () => {
+      const element = await basicFixture();
+      const node = element.shadowRoot.querySelector('.adv-toggle');
+      assert.notOk(node);
+    });
+
+    it('hides advanced fields when has all data', async () => {
+      const element = await basicFixture(createParamsMap());
+      assert.isFalse(element.advancedOpened, 'advanced view is not opened');
+      assert.isTrue(element.isAdvanced, 'isAdvanced is set');
+    });
+
+    it('renders advanced switch when advanced is enabled', async () => {
+      const element = await basicFixture(createParamsMap());
+      const node = element.shadowRoot.querySelector('.adv-toggle');
+      assert.ok(node);
+    });
+
+    it('toggles advanced visibility when switch is clicked', async () => {
+      const element = await basicFixture(createParamsMap());
+      const section = element.shadowRoot.querySelector('.advanced-section');
+      assert.equal(getComputedStyle(section).display, 'none', 'section is hidden');
+      const button = element.shadowRoot.querySelector('.adv-settings-input');
+      tap(button);
+      await nextFrame();
+      assert.equal(getComputedStyle(section).display, 'block', 'section is not hidden');
+    });
+  });
+
+  describe('Data initialization', () => {
+    let element;
+    beforeEach(async () => {
+      element = await basicFixture(createParamsMap());
+    });
+
+    inputFields.forEach(([name, value]) => {
+      it(`input ${name} has value`, async () => {
+        const input = element.shadowRoot.querySelector(`*[name="${name}"]`);
+        assert.equal(input.value, value);
+      });
+    });
+  });
+
+  describe('Change notification', () => {
+    let element;
+    beforeEach(async () => {
+      element = await basicFixture({});
+    });
+
+    inputFields.forEach(([name, value]) => {
+      it(`notifies when ${name} changes`, async () => {
+        const input = element.shadowRoot.querySelector(`*[name="${name}"]`);
+        setTimeout(() => {
+          input.value = value;
+          input.dispatchEvent(new CustomEvent('input'));
+        });
+        const e = await oneEvent(element, 'change');
+        assert.ok(e);
+      });
+    });
+
+    it('does not notify when sets default values', () => {
+      const handler = spy();
+      element.addEventListener('change', handler);
+      element[_setOauth2Defaults]();
+      assert.isFalse(handler.called);
+    });
+
+    it('notifies when changing scopes', async () => {
+      const node = element.shadowRoot.querySelector('oauth2-scope-selector');
+      setTimeout(() => {
+        node.value = ['test'];
+      });
+      const e = await oneEvent(element, 'change');
+      assert.ok(e);
+    });
+  });
+
+  describe('Data serialization', () => {
+    let element;
+    beforeEach(async () => {
+      element = await basicFixture(createParamsMap());
+    });
+
+    inputFields.forEach(([name, value]) => {
+      it(`serialization has ${name}`, async () => {
+        const result = element.serialize();
+        assert.equal(result[name], value);
+      });
+    });
+  });
+
+  describe('Data restoration', () => {
+    let element;
+    let restoreMap;
+
+    beforeEach(async () => {
+      element = await basicFixture();
+      restoreMap = createParamsMap();
+    });
+
+    inputFields.forEach(([name, value]) => {
+      it(`restores ${name}`, () => {
+        element.restore(restoreMap);
+        assert.equal(element[name], value);
+      });
+    });
+  });
+
+  describe('Default values', () => {
+    let element;
+    beforeEach(async () => {
+      element = await basicFixture();
+    });
+
+    it('sets default oauthDeliveryName', () => {
+      assert.equal(element.oauthDeliveryName, 'authorization');
+    });
+
+    it('sets default oauthDeliveryMethod', () => {
+      assert.equal(element.oauthDeliveryMethod, 'header');
+    });
+
+    it('sets grantTypes', () => {
+      assert.deepEqual(element.grantTypes, oauth2GrantTypes);
+    });
+
+    it('sets tokenType', () => {
+      assert.equal(element.tokenType, 'Bearer');
+    });
+  });
+
+  describe('authorization request', () => {
+    let element;
+
+    beforeEach(async () => {
+      element = await basicFixture(createParamsMap());
+    });
+
+    function sendTokenResponse(state, tokenType) {
+      const e = new CustomEvent('oauth2-token-response', {
+        bubbles: true,
+        composed: true,
+        cancelable: true,
+        detail: {
+          accessToken: 'token-value',
+          tokenType,
+          state
+        }
+      });
+      document.body.dispatchEvent(e);
+    }
+
+    inputFields.forEach(([name, value]) => {
+      it(`authorization event has ${name} property`, async () => {
+        const handler = spy();
+        element.addEventListener('oauth2-token-requested', handler);
+        element.authorize();
+        const { detail } = handler.args[0][0];
+        assert.equal(detail[name], value);
+      });
+    });
+
+    it('calls authorize() from button click', () => {
+      const button = element.shadowRoot.querySelector('.auth-button');
+      const handler = spy();
+      element.addEventListener('oauth2-token-requested', handler);
+      tap(button);
+      assert.isTrue(handler.called);
+    });
+
+    it('sets #authorizing flag', () => {
+      element.addEventListener('oauth2-token-requested', (e) => {
+        e.preventDefault();
+      });
+      const button = element.shadowRoot.querySelector('.auth-button');
+      tap(button);
+      assert.isTrue(element.authorizing);
+    });
+
+    it('ignores authorization when did not pass validation', async () => {
+      element.clientId = '';
+      await nextFrame();
+      element.authorize();
+      assert.isFalse(element.authorizing);
+    });
+
+    it('resets the #authorizing flag when token response', async () => {
+      element.addEventListener('oauth2-token-requested', (e) => {
+        e.preventDefault();
+      });
+      element.authorize();
+      await nextFrame();
+      sendTokenResponse();
+      assert.isFalse(element.authorizing);
+    });
+
+    it('sets values from response event', async () => {
+      element.authorize();
+      await nextFrame();
+      sendTokenResponse(undefined, 'other');
+      assert.equal(element.accessToken, 'token-value');
+      assert.equal(element.tokenType, 'other');
+    });
+
+    it('resets the #authorizing flag when token error', async () => {
+      element.authorize();
+      await nextFrame();
+      const e = new CustomEvent('oauth2-error', {
+        bubbles: true,
+        composed: true,
+        cancelable: true,
+        detail: {}
+      });
+      document.body.dispatchEvent(e);
+      assert.isFalse(element.authorizing);
+    });
+  });
+});
